35dd267c14afd56accf7d09e55c89ba59b894c99
CheemsBurguer <strangercheems@gmail.com>
2026-03-03 23:16:06 -0600

    Fix: Insight url problems were fixed

--- src/frontend/account-intelligense/src/pages/accounts/StrategicInsights.tsx
+++ src/frontend/account-intelligense/src/pages/accounts/StrategicInsights.tsx
@@ -57,7 +57,7 @@ export default function StrategicInsightsPage() {
         const fetchCards = async () => {
             setLoading(true);
             try {
-                const res = await fetchWithAuth("/insights/insights/");
+                const res = await fetchWithAuth("/insights/");
                 if (res.ok) {
                     const json = await res.json();
                     setCards(json);
